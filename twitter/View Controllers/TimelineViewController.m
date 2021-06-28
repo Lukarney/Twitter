15c6748aae9eaee88700d8665336cd11350e9a58
Luke Arney <57237540+Lukarney@users.noreply.github.com>
2021-06-28 16:05:51 -0500

    log out function

--- twitter/View Controllers/TimelineViewController.m
+++ twitter/View Controllers/TimelineViewController.m
@@ -8,6 +8,8 @@
 
 #import "TimelineViewController.h"
 #import "APIManager.h"
+#import "LoginViewController.h"
+#import "AppDelegate.h"
 
 @interface TimelineViewController ()
 
@@ -37,9 +39,16 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (void)logOutPush{
-    
+- (IBAction)logOutPushed:(id)sender {
+    [UIApplication sharedApplication].delegate;
+    AppDelegate *appDelegate = (AppDelegate *)[UIApplication sharedApplication].delegate;
+
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    LoginViewController *loginViewController = [storyboard instantiateViewControllerWithIdentifier:@"LoginViewController"];
+    appDelegate.window.rootViewController = loginViewController;
+    [[APIManager shared] logout];
 }
+
 /*
 #pragma mark - Navigation
 
